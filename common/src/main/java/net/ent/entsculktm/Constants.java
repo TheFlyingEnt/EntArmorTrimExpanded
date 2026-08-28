1d07b5ae60ac0d7ea91229239b519e99981c43de
TheFlyingEnt <nihalbey@gmail.com>
2026-08-27 21:16:53 -0400

    Initial commit

---
+++ common/src/main/java/net/ent/entsculktm/Constants.java
@@ -0,0 +1,11 @@
+package net.ent.entsculktm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "entsculktm";
+    public static final String MOD_NAME = "EntSculkTrimMaterial";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
